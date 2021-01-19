f78ad1471d602a35c492b531f87ccf3a2fe393a6
ramankarki <ramankarki40@gmail.com>
2021-01-19 22:08:41 +0545

    initialized cookie and passport global middleware

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require("express");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+const googleAuth = require("./routes/googleAuth");
 
 const app = express();
 
+// global middleware
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    status: "sucess",
-    message: "this is testing",
-  });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// api routes
+app.use("/auth/google", googleAuth);
+
+// serve static build files in production
 
 module.exports = app;
