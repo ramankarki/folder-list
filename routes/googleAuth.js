48899001949ea5ba50b8322f370857cb3ae7d9c0
ramankarki <ramankarki40@gmail.com>
2021-01-19 23:01:03 +0545

    google oauth complete with login and logout route

---
+++ routes/googleAuth.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
+
+const User = require("./../model/Users");
+const router = express.Router();
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleSecretID,
+      callbackURL: "/auth/google/callback",
+      proxy: true,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      console.log(profile);
+      let user = await User.findOne({ googleID: profile.id });
+
+      if (!user) {
+        user = await new User({
+          name: profile._json.given_name,
+          email: profile._json.email,
+          photo: profile._json.picture,
+          googleID: profile.id,
+        }).save();
+      }
+
+      done(null, user);
+    }
+  )
+);
+
+// login
+router.route("/").get(
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+// callback for exchanging code with data
+router.route("/callback").get(passport.authenticate("google"), (req, res) => {
+  res.status(200).json({
+    status: "success",
+    user: req.user,
+  });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.status(200).json({
+    status: "success",
+    message: "you are logged out",
+  });
+});
+
+router.get("/status", (req, res) => {
+  if (!req.user)
+    return res.status(200).json({
+      status: "Logged out!",
+      message: "user is no longer logged in !",
+      statusCode: 401,
+    });
+
+  return res.status(200).json({
+    status: "logged in!",
+    user: req.user,
+    statusCode: 200,
+  });
+});
+
+module.exports = router;
