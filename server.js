25920697c696a2cdd978ba40e9ad33e2dffda0b1
ramankarki <ramankarki40@gmail.com>
2021-01-19 21:55:03 +0545

    server setup with uncaughtException, unhandledRejection handeled and connected mongodb

---
+++ server.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const app = require("./app.js");
+const keys = require("./config/keys");
+
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! Shutting down...");
+  console.log(err);
+  process.exit(1);
+});
+
+mongoose
+  .connect(keys.dbstring, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection successful"));
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () =>
+  console.log(`Server is running at port ${PORT}`)
+);
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! Shutting down...");
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
+});
