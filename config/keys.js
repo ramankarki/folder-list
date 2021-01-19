0e3dd7189532fe8cc9720b6a2901d496cf8c21c9
ramankarki <ramankarki40@gmail.com>
2021-01-19 22:09:05 +0545

    added secret cookie key

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
     dbstring: process.env.DBSTRING,
+    cookieKey: process.env.COOKIE_KEY,
   };
 } else {
   module.exports = require("./dev");
