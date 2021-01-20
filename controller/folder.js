c0e28685dbbd5892f9dfb3154bf7fe10efa40555
ramankarki <ramankarki40@gmail.com>
2021-01-20 10:50:52 +0545

    refractored request handlers with catchAsync function

--- controller/folder.js
+++ controller/folder.js
@@ -1,47 +1,37 @@
 const Folders = require("./../model/Folders");
+const OperationalError = require("./../utils/OperationalError");
+const catchAsync = require("./../utils/catchAsync");
 
-exports.postFolder = async (req, res) => {
-  try {
-    const { title, description, listData } = req.body;
-    const folder = await Folders.create({
-      // userID: req.user.id,
-      userID: 15,
-      title,
-      description,
-      updatedAt: Date.now(),
-      listData,
-    });
-
-    res.status(201).json({
-      status: "success",
-      folder,
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "failed",
-      error: err,
-    });
-  }
-};
-
-exports.getAllFolders = async (req, res) => {
-  try {
-    const folders = await Folders.find({ userID: 15 });
-
-    res.status(200).json({
-      status: "success",
-      folders,
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "failed",
-      err: err,
-    });
-  }
-};
-
-exports.getFolder = async (req, res) => {
-  res.status(200).json({
-    id: req.params.id,
+exports.postFolder = catchAsync(async (req, res, next) => {
+  const { title, description, listData } = req.body;
+  const folder = await Folders.create({
+    // userID: req.user.id,
+    userID: 15,
+    title,
+    description,
+    updatedAt: Date.now(),
+    listData,
   });
-};
+
+  res.status(201).json({
+    status: "success",
+    folder,
+  });
+});
+
+exports.getAllFolders = catchAsync(async (req, res, next) => {
+  const folders = await Folders.find({ userID: 15 });
+
+  res.status(200).json({
+    status: "success",
+    folders,
+  });
+});
+
+exports.getFolder = catchAsync(async (req, res, next) => {
+  const folder = await Folders.findById(req.params.id);
+  res.status(200).json({
+    status: "success",
+    folder,
+  });
+});
