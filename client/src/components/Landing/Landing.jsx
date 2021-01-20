b2a53e792a96d3f23ee541c9ffecd6ef5c2c34da
ramankarki <ramankarki40@gmail.com>
2021-01-20 20:29:16 +0545

    dummy landing page for a while

---
+++ client/src/components/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Landing.scss";
+
+const Landing = () => {
+  return (
+    <h1 className="heading">
+      This is landing page <span>span</span>
+    </h1>
+  );
+};
+
+export default Landing;
