6b136340843995e7cd8bda2aca6cc4ee0effd4f5
ramankarki <ramankarki40@gmail.com>
2021-01-20 13:23:41 +0545

    wired controllers with routes and protected with middleware

--- routes/folder.js
+++ routes/folder.js
@@ -3,13 +3,21 @@ const {
   postFolder,
   getAllFolders,
   getFolder,
+  updateFolder,
+  deleteFolder,
 } = require("./../controller/folder");
+const { protect } = require("./../controller/auth");
 
 const router = express.Router();
 
-router.route("/").post(postFolder).get(getAllFolders);
+// this is protected route, @remaining
+router.route("/").post(protect, postFolder).get(protect, getAllFolders);
 
 // get single folder
-router.route("/:id").get(getFolder);
+router
+  .route("/:id")
+  .get(protect, getFolder)
+  .patch(protect, updateFolder)
+  .delete(protect, deleteFolder);
 
 module.exports = router;
