1c2f691a619ff5e97a3043d9ac948953393acba0
ramankarki <ramankarki40@gmail.com>
2021-01-20 11:08:14 +0545

    initialized globalError handler in app

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+
 const keys = require("./config/keys");
 const googleAuth = require("./routes/googleAuth");
 const folder = require("./routes/folder");
+const globalErrorHandler = require("./controller/globalError");
 
 const app = express();
 
@@ -25,5 +27,8 @@ app.use("/auth/google", googleAuth);
 app.use("/api/v1/folder", folder);
 
 // serve static build files in production
+
+// global error handler
+app.use(globalErrorHandler);
 
 module.exports = app;
