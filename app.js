e080d97dce3d7c6b92673e12d2ada452b0a66974
ramankarki <ramankarki40@gmail.com>
2021-01-20 11:34:13 +0545

    configured morgan and unhandled routes

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const morgan = require("morgan");
 
 const keys = require("./config/keys");
 const googleAuth = require("./routes/googleAuth");
 const folder = require("./routes/folder");
 const globalErrorHandler = require("./controller/globalError");
+const OperationalError = require("./utils/OperationalError");
 
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // global middleware
 app.use(express.json());
@@ -27,6 +33,10 @@ app.use("/auth/google", googleAuth);
 app.use("/api/v1/folder", folder);
 
 // serve static build files in production
+
+app.all("*", (req, res, next) => {
+  next(new OperationalError(404, `Can't find ${req.originalUrl} on this API`));
+});
 
 // global error handler
 app.use(globalErrorHandler);
