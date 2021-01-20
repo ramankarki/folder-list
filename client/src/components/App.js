fb3f8105c37eb3934330e1a9f77cac060519a4f4
ramankarki <ramankarki40@gmail.com>
2021-01-20 20:30:37 +0545

    imported BrowserRouter and Route for / and /dashboard route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Landing from "./Landing/Landing";
+import Header from "./Header/Header";
+import Dashboard from "./Dashboard/Dashboard";
 
 class App extends React.Component {
   render() {
-    return <div>app testing</div>;
+    return (
+      <section>
+        <BrowserRouter>
+          <Header />
+          <Route path="/" exact component={Landing} />
+          <Route path="/dashboard" exact component={Dashboard} />
+        </BrowserRouter>
+      </section>
+    );
   }
 }
 
