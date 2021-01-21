19d19179733f9bcd01e57a8d07b13725cee84e5d
ramankarki <ramankarki40@gmail.com>
2021-01-21 15:07:48 +0545

    created activeDropdown action creator that allows only one dropdown to be active at a time

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,18 @@
 import axios from "axios";
-import { FETCH_USER } from "./types";
+import { FETCH_USER, ACTIVE_ACTION_DROPDOWN } from "./types";
 
 export const fetchUser = () => async (dispatch) => {
   const user = await axios.get("/auth/google/status");
 
   dispatch({ type: FETCH_USER, payload: user.data });
+};
+
+export const activeDropdown = (elementID) => (dispatch, getState) => {
+  const activeDropdown = getState().activeDropdown;
+
+  if (elementID === activeDropdown) {
+    dispatch({ type: ACTIVE_ACTION_DROPDOWN, payload: null });
+  } else {
+    dispatch({ type: ACTIVE_ACTION_DROPDOWN, payload: elementID });
+  }
 };
