67cba370815917b6da0a78ceff5c2de28e265f32
ramankarki <ramankarki40@gmail.com>
2021-01-21 15:08:38 +0545

    stores active dropdown

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import fetchUser from "./fetchUser";
+import activeDropdown from "./activeDropdown";
 
 export default combineReducers({
   user: fetchUser,
+  activeDropdown,
 });
