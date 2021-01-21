2d4ff34f60eb50aa83bad651728811fb487aa4d3
ramankarki <ramankarki40@gmail.com>
2021-01-21 15:10:01 +0545

    wired activeDropdown action creator to deactivate active dropdown element by clicking somewhere else

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 import Landing from "./Landing/Landing";
 import Header from "./Header/Header";
 import Dashboard from "./Dashboard/Dashboard";
 import "./App.scss";
+import { activeDropdown } from "../actions";
 
 class App extends React.Component {
   render() {
     return (
-      <section>
+      <section onClick={() => this.props.activeDropdown("app-container")}>
         <BrowserRouter>
           <Header />
           <Route path="/" exact component={Landing} />
@@ -21,4 +24,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default connect(null, { activeDropdown })(App);
