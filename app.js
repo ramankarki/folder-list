a3460a0c62fb33f3da68cbd9b2e459056822552d
ramankarki <ramankarki40@gmail.com>
2021-01-21 22:45:40 +0545

    removed data sanitiation

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require("express");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const morgan = require("morgan");
-const mongoSanitize = require("express-mongo-sanitize");
-const xss = require("xss-clean");
-const hpp = require("hpp");
 
 const keys = require("./config/keys");
 const googleAuth = require("./routes/googleAuth");
@@ -19,19 +16,6 @@ if (process.env.NODE_ENV === "development") {
 
 // global middleware
 app.use(express.json());
-
-// data sanitization against NoSQL query
-app.use(mongoSanitize());
-
-// data sanitization
-app.use(xss());
-
-// prevent parameter pollution
-app.use(
-  hpp({
-    whitelist: ["duration"],
-  })
-);
 
 app.use(
   cookieSession({
