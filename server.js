8fec2080c6d5451db6b12062d67bd08b21256a91
ramankarki <ramankarki40@gmail.com>
2021-01-21 23:24:49 +0545

    removed dotenv for production

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 
-dotenv.config({ path: "./config.env" });
+// dotenv.config({ path: "./config.env" });
 
 const app = require("./app.js");
 const keys = require("./config/keys");
