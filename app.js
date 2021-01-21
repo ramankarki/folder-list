eae544f165f254eb3cf59450021ace57c60c291c
ramankarki <ramankarki40@gmail.com>
2021-01-21 21:38:09 +0545

    added dependencies to sanitize data

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const morgan = require("morgan");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const keys = require("./config/keys");
 const googleAuth = require("./routes/googleAuth");
@@ -17,6 +20,19 @@ if (process.env.NODE_ENV === "development") {
 
 // global middleware
 app.use(express.json());
+
+// data sanitization against NoSQL query
+app.use(mongoSanitize());
+
+// data sanitization
+app.use(xss());
+
+// prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: ["duration"],
+  })
+);
 
 app.use(
   cookieSession({
