51d5941410a9381c4ff58874ef8ea38dfcaa93d9
ramankarki <ramankarki40@gmail.com>
2021-01-21 15:04:23 +0545

    responsive header with login and logout feature

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { Spinner } from "react-bootstrap";
+
+import Logo from "./favicon.svg";
+import "./Header.scss";
+import { fetchUser, activeDropdown } from "./../../actions/index";
+
+class Header extends React.Component {
+  loginButtonHandler = () => {
+    if (!this.props.user) {
+      return <Spinner animation="border" variant="primary" />;
+    } else if (this.props.user.statusCode === 200) {
+      return (
+        <picture
+          className="userProfile"
+          onClick={(event) => {
+            this.props.activeDropdown("logout-btn");
+            event.stopPropagation();
+          }}
+        >
+          <span className="userEmail">{this.props.user.user.email}</span>
+          <img
+            className="header-img userProfile-img"
+            src={this.props.user.user.photo}
+            alt="user"
+          />
+        </picture>
+      );
+    } else {
+      return (
+        <a href="/auth/google" className="login-button">
+          Login <span className="googleButtonText">with google</span>{" "}
+          <i className="bi bi-google"></i>
+        </a>
+      );
+    }
+  };
+
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <header className="header">
+        <nav className="container d-flex justify-content-between align-items-center pt-2 pb-2">
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <picture className="logo d-flex align-items-center">
+              <img
+                className="header-img logo-img"
+                src={Logo}
+                alt="logo of website"
+              />{" "}
+              Folder List
+            </picture>
+          </Link>
+          <div className="login-logout">
+            {this.loginButtonHandler()}
+            {this.props.activeDropdownState === "logout-btn" ? (
+              <a
+                href="/auth/google/logout"
+                id="logout-btn"
+                className="logout-button"
+              >
+                Logout <span className="triangle"></span>
+              </a>
+            ) : null}
+          </div>
+        </nav>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { user: state.user, activeDropdownState: state.activeDropdown };
+};
+
+export default connect(mapStateToProps, { fetchUser, activeDropdown })(Header);
