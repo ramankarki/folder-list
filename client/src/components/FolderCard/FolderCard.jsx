d448ad7845042e9d3cb665b8e63bb2bdd8fbf0b6
ramankarki <ramankarki40@gmail.com>
2021-01-22 22:12:05 +0545

    responsive FolderCard complete

---
+++ client/src/components/FolderCard/FolderCard.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./FolderCard.scss";
+
+class FolderCard extends React.Component {
+  render() {
+    return (
+      <section className="folderCard">
+        <div className="settings-icon">
+          <i class="bi bi-gear-fill"></i>
+        </div>
+        <h4 className="folderCard-heading">{this.props.heading}</h4>
+        <p className="folderCard-desc">{this.props.desc}</p>
+        <div className="folderCard-date d-flex justify-content-between">
+          <p>
+            Created at
+            <br />
+            {this.props.createdAt}
+          </p>
+          <p>
+            Updated at
+            <br />
+            {this.props.updatedAt}
+          </p>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default FolderCard;
