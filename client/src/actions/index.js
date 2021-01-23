af519437dcdf86cccd2a2f0b5308490963922268
ramankarki <ramankarki40@gmail.com>
2021-01-23 19:40:03 +0545

    action creator for controlled modal fields

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,10 @@
 import axios from "axios";
-import { FETCH_USER, ACTIVE_ACTION_DROPDOWN } from "./types";
+import {
+  FETCH_USER,
+  ACTIVE_ACTION_DROPDOWN,
+  ON_MODAL_TITLE_CHANGE,
+  ON_MODAL_DESC_CHANGE,
+} from "./types";
 
 export const fetchUser = () => async (dispatch) => {
   const user = await axios.get("/auth/google/status");
@@ -14,5 +19,19 @@ export const activeDropdown = (elementID) => (dispatch, getState) => {
     dispatch({ type: ACTIVE_ACTION_DROPDOWN, payload: null });
   } else {
     dispatch({ type: ACTIVE_ACTION_DROPDOWN, payload: elementID });
+  }
+};
+
+export const onModalFieldChange = (type, value) => {
+  if (type === "title") {
+    return {
+      type: ON_MODAL_TITLE_CHANGE,
+      payload: value,
+    };
+  } else if (type === "desc") {
+    return {
+      type: ON_MODAL_DESC_CHANGE,
+      payload: value,
+    };
   }
 };
