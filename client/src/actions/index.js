6a744c282b39249d4572eba07f209e982b681023
ramankarki <ramankarki40@gmail.com>
2021-01-23 21:42:02 +0545

    concatenated new created folder with folders prop of store and dispatch with FETCH_FOLDERS type

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,7 +4,6 @@ import {
   ACTIVE_ACTION_DROPDOWN,
   ON_MODAL_TITLE_CHANGE,
   ON_MODAL_DESC_CHANGE,
-  CREATE_FOLDER,
   FETCH_FOLDERS,
 } from "./types";
 
@@ -38,16 +37,19 @@ export const onModalFieldChange = (type, value) => {
   }
 };
 
-export const createFolder = (title, desc) => async (dispatch) => {
+export const createFolder = (title, desc) => async (dispatch, getState) => {
   const description = desc ? desc : undefined;
 
   const folder = await axios.post("/api/v1/folder", { title, description });
 
-  dispatch({ type: CREATE_FOLDER, payload: folder.data });
+  dispatch({
+    type: FETCH_FOLDERS,
+    payload: [...getState().folders, folder.data.folder],
+  });
 };
 
 export const fetchFolders = () => async (dispatch) => {
   const folders = await axios.get("/api/v1/folder");
 
-  dispatch({ type: FETCH_FOLDERS, payload: folders.data });
+  dispatch({ type: FETCH_FOLDERS, payload: folders.data.folders });
 };
