115f58fc6dacdc70ee69329fd58c5b65e6f589dd
ramankarki <ramankarki40@gmail.com>
2021-01-23 19:40:43 +0545

    reducers for storing state of modal fields

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from "redux";
 import fetchUser from "./fetchUser";
 import activeDropdown from "./activeDropdown";
+import { onModalDescChange, onModalTitleChange } from "./onModalFieldChange";
 
 export default combineReducers({
   user: fetchUser,
   activeDropdown,
+  modalTitle: onModalTitleChange,
+  modalDesc: onModalDescChange,
 });
