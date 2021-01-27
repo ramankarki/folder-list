c0fb2c56e3dae68d2c94a3235d8c29b2f0994a89
ramankarki <ramankarki40@gmail.com>
2021-01-27 13:44:04 +0545

    created isFolderRequestLoading and exitCreateUpdateModal helper function

--- client/src/actions/helper.js
+++ client/src/actions/helper.js
@@ -1,4 +1,11 @@
-import { ERROR } from "./types";
+import {
+  ERROR,
+  FOLDER_REQUEST_LOADING,
+  ON_MODAL_TITLE_CHANGE,
+  ON_MODAL_DESC_CHANGE,
+  ACTIVE_ACTION_DROPDOWN,
+  FOLDER_MODAL_STATE,
+} from "./types";
 
 export const errorCreator = (heading, text, dispatch) => {
   dispatch({
@@ -7,5 +14,31 @@ export const errorCreator = (heading, text, dispatch) => {
       heading,
       text,
     },
+  });
+};
+
+export const folderRequestLoading = (isLoading, dispatch) => {
+  dispatch({
+    type: FOLDER_REQUEST_LOADING,
+    payload: isLoading,
+  });
+};
+
+export const exitCreateUpdateFolder = (dispatch) => {
+  dispatch({
+    type: ON_MODAL_TITLE_CHANGE,
+    payload: "",
+  });
+  dispatch({
+    type: ON_MODAL_DESC_CHANGE,
+    payload: "",
+  });
+  dispatch({
+    type: ACTIVE_ACTION_DROPDOWN,
+    payload: "exit-modal-btn",
+  });
+  dispatch({
+    type: FOLDER_MODAL_STATE,
+    payload: "",
   });
 };
