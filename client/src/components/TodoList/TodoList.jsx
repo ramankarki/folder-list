f42b554fe20232bd16aad62cc81ed514867f2507
ramankarki <ramankarki40@gmail.com>
2021-01-28 17:54:40 +0545

    made final look of todoList page with heading, desc, statefull tabs, add todo-item form and imported todo-item component

--- client/src/components/TodoList/TodoList.jsx
+++ client/src/components/TodoList/TodoList.jsx
@@ -1,9 +1,72 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchFolder } from "../../actions";
+
+import TodoItem from "../TodoItem/TodoItem";
+import "./TodoList.scss";
 
 class TodoList extends React.Component {
+  state = { activeTab: "All" };
+
+  activeTab = (tab) => {
+    if (tab === this.state.activeTab) return "tabs tabs-active";
+    return "tabs";
+  };
+
+  setActiveTab = (event) =>
+    this.setState({ activeTab: event.target.textContent });
+
+  onTodoItemSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  componentDidMount() {
+    this.props.fetchFolder(null, this.props.match.params.id);
+  }
+
   render() {
-    return <div>this is todo list</div>;
+    return (
+      <section className="todo-list-route container">
+        <header>
+          <h1 className="todo-title">Shooping list</h1>
+          <p className="todo-desc">this is desc of this todo list</p>
+        </header>
+        <section className="todo-list">
+          <header className="todo-list-tabs">
+            <button
+              className={this.activeTab("All")}
+              onClick={this.setActiveTab}
+            >
+              All
+            </button>
+            <button
+              className={this.activeTab("Pending")}
+              onClick={this.setActiveTab}
+            >
+              Pending
+            </button>
+            <button
+              className={this.activeTab("Completed")}
+              onClick={this.setActiveTab}
+            >
+              Completed
+            </button>
+          </header>
+          <form onSubmit={this.onTodoItemSubmit} className="todo-form">
+            <input type="text" placeholder="Add Item" />
+            <button>Add</button>
+          </form>
+          <section className="todo-items">
+            <TodoItem />
+          </section>
+        </section>
+      </section>
+    );
   }
 }
 
-export default TodoList;
+const mapStateToProps = (state) => {
+  return { activeTodoList: state.activeTodoList };
+};
+
+export default connect(mapStateToProps, { fetchFolder })(TodoList);
