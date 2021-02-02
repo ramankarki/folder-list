27c264f93ee9f9731dd67812c0b266c4c16cb04c
ramankarki <ramankarki40@gmail.com>
2021-02-02 20:08:10 +0545

    main responsive hero section

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import illustration from "./illustration.svg";
 import "./Landing.scss";
 
 const Landing = () => {
   return (
-    <h1 className="heading">
-      This is landing page <span>span</span>
-    </h1>
+    <section className="landing-page">
+      <div className="landing-page-wrapper container">
+        <article className="hero1">
+          <picture>
+            <img src={illustration} alt="todo items inside folder" />
+          </picture>
+          <div className="hero1-readings">
+            <h1 className="hero1-heading">
+              Separate your Todo list in Folders
+            </h1>
+            <p className="hero1-desc">
+              You can keep your list of items separate with folders
+            </p>
+            <a href="/auth/google" className="hero1-ctaBtn">
+              Get Started
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
   );
 };
 
