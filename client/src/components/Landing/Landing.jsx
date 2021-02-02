259285e8a1dc10bd2dbfb09ce7993440181158df
ramankarki <ramankarki40@gmail.com>
2021-02-02 20:35:34 +0545

    second hero section

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import illustration from "./illustration.svg";
+import illustration2 from "./illustration-2.svg";
 import "./Landing.scss";
 
 const Landing = () => {
@@ -20,6 +21,18 @@ const Landing = () => {
             <a href="/auth/google" className="hero1-ctaBtn">
               Get Started
             </a>
+          </div>
+        </article>
+        <article className="hero2">
+          <picture>
+            <img src={illustration2} alt="todo items inside folder" />
+          </picture>
+          <div className="hero2-readings">
+            <h1 className="hero2-heading">Access from anywhere</h1>
+            <p className="hero2-desc">
+              The ability to use a smartphone, tablet, or computer to access
+              your account means your files follow you everywhere.
+            </p>
           </div>
         </article>
       </div>
